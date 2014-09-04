878223054b85b7410bcbd61f4e947e7a19fb2df9
NestorArielOcampoVidaurre <nestor_90.ooo@hotmail.com>
2014-09-04 18:14:48 -0500

    ejercicio4

---
+++ practica4/ejercicio4.py
@@ -0,0 +1,120 @@
+
+#lista=range(1,21)
+#print lista
+#raw_input()
+
+#segundo ejercicio
+
+#lista=range(20, 0,-1)
+#print lista
+#raw_input()
+
+
+#tercer ejercicio
+
+#    return a + b
+#num1 =raw_input("Num1: ")
+#num2 = raw_input("Num2: ")
+#print("Opcion\n1.- Sumar")
+#operacion = { '1': sumar}
+#seleccion = raw_input('la operacion es : ')
+#try:
+ #   resultado = operaciones[seleccion](int(num1), int(num2))
+  #  print resultado
+#except:
+ #   print("Eso no vale")
+#raw_input()
+
+
+
+#cuarto ejercicio
+
+#def suma2(a, b):
+ #   return a,b
+#num1 =raw_input("Num1: ")
+#num2 = raw_input("Num2: ")
+#print("Opcion\n1.- Concatenar")
+#operaciones = { '1': suma2,}
+#seleccion = raw_input('seleccionar : ')
+#try:
+ #   resultado = operaciones[seleccion](int(num1), int(num2))
+  #  print resultado    
+#except:
+ #   print("Eso no vale")
+#raw_input()
+
+
+
+#quinto ejercicio
+
+#def suma3(a, b):
+ #   return a+b
+#num1 =raw_input("Num1: ")
+#num2 = float (raw_input("Num: "))
+#print("Opcion\n1.- sumar\n2.- salir")
+#operaciones = { '1': suma3,}
+#seleccion = raw_input('seleccionar : ')
+#try:
+#    resultado = operaciones[seleccion](int(num1), abs(num2 - int(num2)))
+#    print resultado    
+#except:
+#    print("gracias")
+#raw_input()
+
+
+#ejercicio nro 2
+
+#num =int(raw_input("Num2: "))
+#for i in range(num):
+#	if i%2==0:
+#		print ' ' +str(i),
+#raw_input()
+
+
+#ejercicio nro 3
+#print"inserte una palabra"
+#def es_palindromo (cadena):
+#    pal_invertida = inversa (cadena)
+#    ind = 0
+ #   cont = 0
+  #  for i in range (len(cadena)):
+   #     if pal_invertida[indice] == cadena[ind]:
+    #        ind =ind+1
+     #       cont =cont+1
+      #  else:
+       #     print "False"
+        #    break
+    	#if cont == len(cadena): 
+        #	print "True"
+#raw_input()
+
+
+#ejercicio nro 4
+palabra1 = raw_input("Palabra 1: ")
+palabra2 = raw_input("Palabra 2: ")
+largo_pal1 = len(palabra1)                      
+largo_pal2 = len(palabra2)                      
+dif_largo = abs(largo_pal1 - largo_pal2)    
+if largo_pal1 > largo_pal2:                 
+    print "La palabra es : ", palabra1     
+elif largo_pal1 < largo_pal2:                
+    print "La palabra es: ", palabra2    
+else:                                               
+    print "La palabra es : ", palabra1+palabra2
+raw_input() 
+
+
+
+
+
+#print"			   TABLA DE DIVISION "
+#print"1/1=1"
+#raw_input()
+
+
+#num=int(raw_input("Ingrese un numero: "))
+#var=1
+#if var<=num:
+#	print var
+#	var=var+1
+#raw_input()
